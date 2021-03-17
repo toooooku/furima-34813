07d145ce15c977dbb251242ff1e1e9dd7fea34c3
toooooku <79389139+toooooku@users.noreply.github.com>
2021-03-17 18:20:01 +0900

    コメントアウト及びバリデーションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,24 +2,24 @@ class ItemsController < ApplicationController
   def index
   end
 
-  def new
-    @items = Item.new
-  end
+  #def new
+   #@items = Item.new
+  #end
   
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end  
-  end
+  #def create
+    #@item = Item.new(item_params)
+    #if @item.save
+      #redirect_to root_path
+    #else
+      #render :new
+    #end  
+  #end
   
-  private
+  #private
 
-  def item_params
-    params.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
-  end
+  #def item_params
+    #params.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+  #end
 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,12 @@ class User < ApplicationRecord
   has_many :items
   has_many :destinations
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
-
   with_options presence: true do
+    validates :nickname, presence: true
     # ひらがな、カタカナ、漢字のみ許可する
     validates :family_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
     # カタカナのみ許可する
     validates :family_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birth_day, presence: true
   end
 end
