f67f215239867b57f5e2c7381081839b0ceab4f0
toooooku <79389139+toooooku@users.noreply.github.com>
2021-03-17 12:37:55 +0900

    ユーザー名全角入力必須の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ApplicationRecord
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birth_day, presence: true
+
+  with_options presence: true do
+    # ひらがな、カタカナ、漢字のみ許可する
+    validates :family_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."}
+    # カタカナのみ許可する
+    validates :family_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  end
 end
