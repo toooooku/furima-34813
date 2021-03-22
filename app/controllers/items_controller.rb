7823f287865d41befe0dcb537144aa88c9112870
toooooku <79389139+toooooku@users.noreply.github.com>
2021-03-22 12:29:32 +0900

    商品削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :set_refactoring, only: [:edit, :update]
+  before_action :set_refactoring, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
